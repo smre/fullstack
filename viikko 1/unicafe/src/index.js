e9dfcbde24f6cc9a2f384c41a5bb6a0d88de00c7
sumire <me@sumire.io>
2018-01-28 18:09:04 +0200

    Add exercise 1.8

--- viikko 1/unicafe/src/index.js
+++ viikko 1/unicafe/src/index.js
@@ -7,6 +7,29 @@ const Header = ({header}) => (
   </div>
 );
 
+const Button = ({handleClick, text}) => (
+  <button onClick={handleClick}>
+    {text}
+  </button>
+);
+
+const Statistics = ({average, positive}) => {
+  return (
+    <div>
+      <Statistic name="Keskiarvo" value={average}/>
+      <Statistic name="Positiivisia" value={positive}/>
+    </div>
+  );
+};
+
+const Statistic = ({name, value}) => {
+  return (
+    <div>
+      {name} {value}
+    </div>
+  );
+};
+
 class App extends React.Component {
   constructor (props) {
     super(props);
@@ -20,30 +43,35 @@ class App extends React.Component {
     };
   }
 
-  updateAverage = () => {
+  updateAverage = function (input) {
     let sum = 0;
-    for (let i = 0; i < this.state.kaikki.length; i++) {
-      sum += this.state.kaikki[i];
+    for (let i = 0; i < input.length; i++) {
+      sum += input[i];
     }
-    return sum / this.state.kaikki.length;
+    return sum / input.length;
   };
 
-  updatePositive = () => {
-    const positive = this.state.kaikki.filter(function (i) {return i === 1;});
-    return positive.length / this.state.kaikki.length;
+  updatePositive = function (input) {
+    const positive = input.filter(function (i) {return i === 1;});
+    return positive.length / input.length;
   };
 
   clickHyva = () => {
     this.setState({
       hyva: this.state.hyva + 1,
       kaikki: this.state.kaikki.concat(1),
+      average: this.updateAverage(this.state.kaikki.concat(1)),
+      positive: this.updatePositive(this.state.kaikki.concat(1)),
     });
+
   };
 
   clickNeutraali = () => {
     this.setState({
       neutraali: this.state.neutraali + 1,
       kaikki: this.state.kaikki.concat(0),
+      average: this.updateAverage(this.state.kaikki.concat(0)),
+      positive: this.updatePositive(this.state.kaikki.concat(0)),
     });
   };
 
@@ -51,6 +79,8 @@ class App extends React.Component {
     this.setState({
       huono: this.state.huono + 1,
       kaikki: this.state.kaikki.concat(-1),
+      average: this.updateAverage(this.state.kaikki.concat(-1)),
+      positive: this.updatePositive(this.state.kaikki.concat(-1)),
     });
   };
 
@@ -60,15 +90,27 @@ class App extends React.Component {
       <div>
         <div>
           <Header header={'anna palautetta'}/>
-          <button onClick={this.clickHyva}>hyvä</button>
-          <button onClick={this.clickNeutraali}>neutraali</button>
-          <button onClick={this.clickHuono}>huono</button>
+          <Button
+            handleClick={this.clickHyva}
+            text='Hyvä'
+          />
+          <Button
+            handleClick={this.clickNeutraali}
+            text='Neutraali'
+          />
+          <Button
+            handleClick={this.clickHuono}
+            text='Huono'
+          />
           <Header header={'statistiikka'}/>
           Hyvä: {this.state.hyva}<br/>
           Neutraali: {this.state.neutraali}<br/>
           Huono: {this.state.huono}<br/>
-          Keskiarvo: {this.updateAverage()}<br/>
-          Positiivisia: {this.updatePositive()}
+
+          <Statistics
+            average={this.state.average}
+            positive={this.state.positive}
+          />
         </div>
       </div>
     );
