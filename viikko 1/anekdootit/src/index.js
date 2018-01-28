d4b8dafda03e03d8fe182d1592ed9fd59fe2705c
sumire <me@sumire.io>
2018-01-28 23:39:35 +0200

    Add exercise 1.13

--- viikko 1/anekdootit/src/index.js
+++ viikko 1/anekdootit/src/index.js
@@ -1,27 +1,51 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
+import React from 'react';
+import ReactDOM from 'react-dom';
 
 class App extends React.Component {
-  constructor(props) {
-    super(props)
+  constructor (props) {
+    super(props);
     this.state = {
       selected: 0,
-    }
+      votes: [0, 0, 0, 0, 0, 0],
+    };
   }
 
-  randomAnecdote() {
+  randomAnecdote = () => {
     return () => {
-      this.setState({selected: Math.floor(Math.random() * anecdotes.length)})
-    }
-  }
+      this.setState({
+        selected: Math.floor(Math.random() * anecdotes.length),
+      });
+    };
+  };
 
-  render() {
+  vote = () => {
+    return () => {
+      let points = this.state.votes;
+      if (isNaN(points[this.state.selected])) {
+        points[this.state.selected] = 1;
+      } else {
+        points[this.state.selected] += 1;
+      }
+
+      this.setState({
+        votes: points,
+      });
+    };
+  };
+
+  render () {
+    const selected = this.state.selected;
+    const votes = this.state.votes;
+    console.log(selected);
+    console.log(votes);
     return (
       <div>
-        {this.props.anecdotes[this.state.selected]}<br/>
+        {this.props.anecdotes[selected]}<br/>
+        has {votes[selected]} votes<br/>
+        <button onClick={this.vote()}>vote</button>
         <button onClick={this.randomAnecdote()}>next anecdote</button>
       </div>
-    )
+    );
   }
 }
 
@@ -31,10 +55,10 @@ const anecdotes = [
   'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
   'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
   'Premature optimization is the root of all evil.',
-  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
-]
+  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+];
 
 ReactDOM.render(
-  <App anecdotes={anecdotes} />,
-  document.getElementById('root')
-)
+  <App anecdotes={anecdotes}/>,
+  document.getElementById('root'),
+);
