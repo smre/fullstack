b9ddf6b35914f47c9842ecc564ddd0f3eb935a8c
sumire <me@sumire.io>
2018-01-28 17:41:01 +0200

    Add exercise 1.7

--- viikko 1/unicafe/src/index.js
+++ viikko 1/unicafe/src/index.js
@@ -14,24 +14,43 @@ class App extends React.Component {
       hyva: 0,
       neutraali: 0,
       huono: 0,
+      kaikki: [],
+      average: 0,
+      positive: 0,
     };
   }
+
+  updateAverage = () => {
+    let sum = 0;
+    for (let i = 0; i < this.state.kaikki.length; i++) {
+      sum += this.state.kaikki[i];
+    }
+    return sum / this.state.kaikki.length;
+  };
+
+  updatePositive = () => {
+    const positive = this.state.kaikki.filter(function (i) {return i === 1;});
+    return positive.length / this.state.kaikki.length;
+  };
 
   clickHyva = () => {
     this.setState({
       hyva: this.state.hyva + 1,
+      kaikki: this.state.kaikki.concat(1),
     });
   };
 
   clickNeutraali = () => {
     this.setState({
       neutraali: this.state.neutraali + 1,
+      kaikki: this.state.kaikki.concat(0),
     });
   };
 
   clickHuono = () => {
     this.setState({
       huono: this.state.huono + 1,
+      kaikki: this.state.kaikki.concat(-1),
     });
   };
 
@@ -47,7 +66,9 @@ class App extends React.Component {
           <Header header={'statistiikka'}/>
           Hyvä: {this.state.hyva}<br/>
           Neutraali: {this.state.neutraali}<br/>
-          Huono: {this.state.huono}
+          Huono: {this.state.huono}<br/>
+          Keskiarvo: {this.updateAverage()}<br/>
+          Positiivisia: {this.updatePositive()}
         </div>
       </div>
     );
