1e310a34d5f966e38d49c9fc5ab1be00d23162fd
sumire <me@sumire.io>
2018-01-29 00:04:10 +0200

    Add exercise 1.14

--- viikko 1/anekdootit/src/index.js
+++ viikko 1/anekdootit/src/index.js
@@ -1,11 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+const Highest = ({selected, highest}) => {
+  if (highest.votes === 0) {
+    return (
+      <div>
+        Not enough votes yet
+      </div>
+    )
+  } else {
+    return (
+      <div>
+        {anecdotes[highest.index]}<br/>
+        has {highest.votes} {highest.votes === 1 ? 'vote' : 'votes'}
+      </div>
+    )
+  }
+
+}
+
 class App extends React.Component {
   constructor (props) {
     super(props);
     this.state = {
       selected: 0,
+      highest: {index: 0, votes: 0},
       votes: [0, 0, 0, 0, 0, 0],
     };
   }
@@ -21,14 +40,21 @@ class App extends React.Component {
   vote = () => {
     return () => {
       let points = this.state.votes;
+      let highest = this.state.highest;
       if (isNaN(points[this.state.selected])) {
         points[this.state.selected] = 1;
       } else {
         points[this.state.selected] += 1;
       }
 
+      if (points[this.state.selected] > this.state.highest.votes) {
+        highest.index = this.state.selected;
+        highest.votes = points[this.state.selected];
+      }
+
       this.setState({
         votes: points,
+        highest: highest
       });
     };
   };
@@ -36,14 +62,14 @@ class App extends React.Component {
   render () {
     const selected = this.state.selected;
     const votes = this.state.votes;
-    console.log(selected);
-    console.log(votes);
     return (
       <div>
         {this.props.anecdotes[selected]}<br/>
         has {votes[selected]} votes<br/>
         <button onClick={this.vote()}>vote</button>
         <button onClick={this.randomAnecdote()}>next anecdote</button>
+        <h2>anecdote with most votes</h2><br/>
+        <Highest selected={this.state.selected} highest={this.state.highest} />
       </div>
     );
   }
