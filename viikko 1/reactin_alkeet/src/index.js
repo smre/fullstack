16d14b1a0a6cb5cac959e9849845538977fc4c13
sumire <eino.keningi@gmail.com>
2018-02-03 19:01:11 +0200

    Add exercise 2.5

--- viikko 1/reactin_alkeet/src/index.js
+++ viikko 1/reactin_alkeet/src/index.js
@@ -1,55 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Kurssi from './components/Kurssi'
-
-const App = () => {
-  const kurssit = [
-    {
-      nimi: 'Half Stack -sovelluskehitys',
-      id: 1,
-      osat: [
-        {
-          nimi: 'Reactin perusteet',
-          tehtavia: 10,
-          id: 1
-        },
-        {
-          nimi: 'Tiedonvälitys propseilla',
-          tehtavia: 7,
-          id: 2
-        },
-        {
-          nimi: 'Komponenttien tila',
-          tehtavia: 14,
-          id: 3
-        }
-      ]
-    },
-    {
-      nimi: 'Node.js',
-      id: 2,
-      osat: [
-        {
-          nimi: 'Routing',
-          tehtavia: 3,
-          id: 1
-        },
-        {
-          nimi: 'Middlewaret',
-          tehtavia: 7,
-          id: 2
-        }
-      ]
-    }
-  ]
-
-  return (
-    <div>
-      <Kurssi kurssit={kurssit} />
-    </div>
-  )
-}
-
+import App from './App'
 
 ReactDOM.render(
   <App/>,
