153d71b9e9c0e0b5fd3db3430c3d3002840e7fd4
sumire <eino.keningi@gmail.com>
2018-02-03 20:20:47 +0200

    Add exercise 2.8

--- viikko 2/puhelinluettelo/src/App.js
+++ viikko 2/puhelinluettelo/src/App.js
@@ -1,9 +1,9 @@
 import React from 'react';
 
-const Name = ({name}) => {
+const Name = ({name, number}) => {
   return (
     <li>
-      {name}
+      {name} {number}
     </li>
   );
 };
@@ -15,29 +15,33 @@ class App extends React.Component {
       persons: [
         {
           name: 'Arto Hellas',
+          number: '123123789',
           id: 1,
         },
       ],
       newName: '',
+      newNumber: '',
     };
   }
 
-  addName = (event) => {
+  addPerson = (event) => {
     event.preventDefault();
 
     if (this.state.persons.some(person => person.name === this.state.newName)) {
       console.warn("I'm afraid I can't let you do that, Dave.");
     } else {
-      const nameObject = {
+      const personObject = {
         name: this.state.newName,
+        number: this.state.newNumber,
         id: this.state.persons.length + 1,
       };
 
-      const persons = this.state.persons.concat(nameObject);
+      const persons = this.state.persons.concat(personObject);
 
       this.setState({
         persons: persons,
         newName: '',
+        newNumber: '',
       });
     }
   };
@@ -46,17 +50,29 @@ class App extends React.Component {
     this.setState({newName: event.target.value});
   };
 
+  handleNumberChange = (event) => {
+    this.setState({newNumber: event.target.value});
+  };
+
   render () {
     const personsToShow = this.state.persons;
 
     return (
       <div>
         <h2>Puhelinluettelo</h2>
-        <form onSubmit={this.addName}>
+        <form onSubmit={this.addPerson}>
           <div>
             <input
+              placeholder="Nimi"
               value={this.state.newName}
               onChange={this.handleNameChange}
+            />
+          </div>
+          <div>
+            <input
+              placeholder="Puhelinnumero"
+              value={this.state.newNumber}
+              onChange={this.handleNumberChange}
             />
           </div>
           <div>
@@ -66,7 +82,7 @@ class App extends React.Component {
         <h2>Numerot</h2>
         <ul>
           {personsToShow.map(
-            person => <Name key={person.id} name={person.name}/>)}
+            person => <Name key={person.id} name={person.name} number={person.number}/>)}
         </ul>
       </div>
     );
