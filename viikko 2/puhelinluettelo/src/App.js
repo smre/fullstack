512c81904565048132da3ebfe67f9724fc79301b
sumire <eino.keningi@gmail.com>
2018-02-03 19:08:50 +0200

    Remove unneeded files

--- viikko 2/puhelinluettelo/src/App.js
+++ viikko 2/puhelinluettelo/src/App.js
@@ -1,21 +1,33 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-class App extends Component {
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      persons: [
+        { name: 'Arto Hellas' }
+      ],
+      newName: ''
+    }
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <h2>Puhelinluettelo</h2>
+        <form>
+          <div>
+            nimi: <input />
+          </div>
+          <div>
+            <button type="submit">lisää</button>
+          </div>
+        </form>
+        <h2>Numerot</h2>
+        ...
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
